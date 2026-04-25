f3ecb736dadd84a86c610ab5edfddd9138b346bc
Anonymas-Doge <keenanbrendan7@gmail.com>
2026-04-25 00:35:24 -0700

    convert discord to uv package managing

--- discord/cogs/operation.py
+++ discord/cogs/operation.py
@@ -52,6 +52,38 @@ class Operation(commands.Cog, name="operation"):
             description="The scope must be `global` or `guild`.", color=0xE02B2B
         )
         await context.send(embed=embed)
+        
+    @commands.command(
+        name="unsync",
+        description="Unsynchonizes the slash commands.",
+    )
+    @app_commands.describe(
+        scope="The scope of the sync. Can be `global`, `current_guild` or `guild`"
+    )
+    @commands.is_owner()
+    async def unsync(self, context: Context, scope: str) -> None:
+        if scope == "global":
+            context.bot.tree.clear_commands(guild=None)
+            await context.bot.tree.sync()
+            embed = discord.Embed(
+                description="Slash commands have been globally unsynchronized.",
+                color=0xBEBEFE,
+            )
+            await context.send(embed=embed)
+            return
+        elif scope == "guild":
+            context.bot.tree.clear_commands(guild=context.guild)
+            await context.bot.tree.sync(guild=context.guild)
+            embed = discord.Embed(
+                description="Slash commands have been unsynchronized in this guild.",
+                color=0xBEBEFE,
+            )
+            await context.send(embed=embed)
+            return
+        embed = discord.Embed(
+            description="The scope must be `global` or `guild`.", color=0xE02B2B
+        )
+        await context.send(embed=embed)
 
 
 async def setup(bot) -> None:
