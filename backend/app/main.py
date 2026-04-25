ac1e6573ee1aea51a354dcb294df44a0107fc689
EfinZhao <ethan.zhao.us@gmail.com>
2026-04-25 09:11:01 -0700

    A few changes to the backend needed to ensure it actually works

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,12 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import SQLModel
 
 from app.api.main import api_router
 from app.core import stripe_client  # noqa: F401
+from app.core.config import settings
 from app.core.database import engine
 
 
@@ -17,5 +19,13 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[settings.FRONTEND_BASE_URL],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 app.include_router(api_router, prefix='/api')
